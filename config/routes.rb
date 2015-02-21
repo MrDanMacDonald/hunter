3701d5edcd4b442248e81624fd30455fb5deed56
MrDanMacDonald <dan@danmacd.com>
2015-02-21 11:54:48 -0800

    modified styling on postsINDEX

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,11 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   resources :users
-<<<<<<< Updated upstream
   resources :posts do 
     member do
       put 'like', to: 'posts#upvote'
     end
   end
-=======
-  resources :posts
->>>>>>> Stashed changes
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
