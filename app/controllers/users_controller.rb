0a883219668a3af7bd2e796246dccc5a4424ca7c
MrDanMacDonald <dan@danmacd.com>
2015-03-14 22:45:02 -0700

    add post from user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @upvotedPosts = @user.find_up_voted_items
+    @post = @user.posts.new
   end
 
   def new
