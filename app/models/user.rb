10377bd9784951a919b2ab52fd81080d8e54bcca
MrDanMacDonald <dan@danmacd.com>
2015-03-22 13:46:09 -0700

    added some tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ActiveRecord::Base
 
   has_many :posts
   has_many :comments
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
 
   acts_as_voter
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,21 @@ require 'spec_helper'
 
 describe User do
   it { should have_many(:posts) }
+  it { should have_many(:comments) }
+  it { should have_many(:active_relationships) }
 
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:email) }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:photo) }
+  it { should respond_to(:title) }
+  it { should respond_to(:provider) }
+  it { should respond_to(:description) }
+  it { should respond_to(:location) }
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  it "should be valid" do
+    expect(user).to be_valid
+  end
 end
