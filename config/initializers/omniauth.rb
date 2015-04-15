16587cd8f6e21f08d27cfc4834a4dd0847b934d5
Rafe Hatfield <rafe.hatfield@gmail.com>
2015-04-15 09:06:43 -0700

    update to credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 # TODO: Move keys into a YML file
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret,
+  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'],
   {
     image_size: 'original',
     authorize_params: {
